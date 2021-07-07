bd057509be2edf5f0d9ccd2b88f1d6656ce0a0a8
Duncan McLean <duncan.mclean@ibm.com>
2021-07-07 10:10:18 -0400

    Header positioning updated to flexbox

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,18 +3,27 @@ import React from 'react'
 const Contact = () => {
     return (
         <div
+        className = 'container'
         style = {{
             padding: 200,
-            backgroundColor:'#88BBD6'
+            backgroundColor:'#88BBD6',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            flexWrap: 'wrap'
+            
         }}
         >
-            Contact Page
+            
+        <div>Column 1</div>
+        <div>Column 2</div>
+
         </div>
     )
 }
 
 export default Contact
-// <a href='mailto:duncanmclean12@gmail.com?subject = Feedback&body = Message' 
+// <a href='mailto:duncanmclean97@gmail.com?subject = Feedback&body = Message' 
 // style = {{display: 'block'}}>
 // Email
 // </a>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,33 @@ import Button from './Button'
 
 const Header = () => {
     return (
-        <div style = {{backgroundColor: '#CDCDCD', position: 'sticky', top: 0, zIndex: 1}}>
+        <div className = "container" style = {{backgroundColor: '#CDCDCD', position: 'sticky', top: 0, zIndex: 1, display: 'flex', flexDirection: 'row',
+                                                justifyContent: 'center', flexWrap: 'wrap'}}>
            
             {/* Needs to be changed to the "a" tag, link to top of the page */}
-            <h1 style = {{textAlign: 'center'}} >-Duncan McLean-</h1>
+            <h1 style = {{alignSelf: 'center' }} >-Duncan McLean-</h1>
             
 
             {/* Button postioning */}
-            <div style = {
-                {position: 'fixed', 
-                display: 'block', 
-                zIndex: 1, 
-                right: 60, 
-                top: 15}
+            <h1 style = {
+                {alignSelf: 'flex-end', position: 'fixed', zIndex: 1, right: 60, top: 0}
             }> 
             <Button
                 text = "☰"
                 color = "#CDCDCD"
             />
-            </div>
+            </h1>
 
         </div>
     )
 }
 
 export default Header
+
+/*
+{position: 'fixed', 
+                display: 'block', 
+                zIndex: 1, 
+                right: 60, 
+                top: 15}
+*/
